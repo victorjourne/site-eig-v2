25fc6d6dae04a36c4e6478dd7469cd4a6fe0b3c2
Julien Siwek <julien.siwek@gmail.com>
2021-04-27 11:18:07 +0200

    fix: eig-16

--- src/js/grid.js
+++ src/js/grid.js
@@ -2,18 +2,49 @@ const Isotope = require('isotope-layout');
 
 export default class Grid {
   constructor() {
-     this.setupIsotope();
+    this.resized = true;
+    this.timeout = null;
+    this.setupIsotope();
   }
  
   setupIsotope() {
-    const $isotope = document.querySelector('.js-isotope');
+    const $isotope = document.querySelector('.js-grid');
 
     if(!$isotope) return;
 
-    this.isotope = new Isotope($isotope);
+    // window.addEventListener('resize', () => this.resized = true);
+    this.setEqualHeight(); 
+
+    this.isotope = new Isotope($isotope, {
+      layoutMode: 'fitRows',
+    });
 
     document.querySelector('.js-filter').addEventListener('change', (e) => {
       this.filter(e.target.value);
+    });
+  }
+
+  // refresh() {
+  //   console.log('refr');
+  //   if(this.resized) {
+  //     requestAnimationFrame(this.setEqualHeight);
+  //   }
+  //   clearTimeout(this.timeout);
+  //   this.timeout = setTimeout(this.refresh, 100);
+  //   this.resized = false;
+  // }
+
+  setEqualHeight() {
+    const cards = document.querySelectorAll('.js-grid .fr-card');
+    var elems = [].slice.call(cards);
+
+    var tallest = Math.max.apply(Math, elems.map(function(elem, index) {
+      elem.style.minHeight = ''; // clean first
+      return elem.offsetHeight;
+    }));
+  
+    elems.forEach(function(elem, index, arr){ 
+      elem.style.minHeight = tallest + 'px';
     });
   }
 
