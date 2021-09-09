90dafc6d3189f3c23743c008d438d53d0e8530c2
Julien Siwek <julien.siwek@gmail.com>
2021-09-09 10:37:14 +0200

    fix: eig-139

--- src/js/grid.js
+++ src/js/grid.js
@@ -1,24 +1,24 @@
-const Isotope = require('isotope-layout');
+const Isotope = require("isotope-layout");
 
 export default class Grid {
   constructor() {
     this.resized = true;
     this.timeout = null;
-    this.$filters = document.querySelectorAll('.js-filter');
+    this.$filters = document.querySelectorAll(".js-filter");
 
     this.setupIsotope();
   }
- 
-  setupIsotope() {
-    const $isotope = document.querySelector('.js-grid');
 
-    if(!$isotope) return;
+  setupIsotope() {
+    const $isotope = document.querySelector(".js-grid");
+
+    if (!$isotope) return;
 
     // window.addEventListener('resize', () => this.resized = true);
-    this.setEqualHeight(); 
+    this.setEqualHeight();
 
     this.isotope = new Isotope($isotope, {
-      layoutMode: 'fitRows',
+      layoutMode: "fitRows",
     });
 
     // document.querySelector('.js-filter').addEventListener('change', (e) => {
@@ -26,8 +26,8 @@ export default class Grid {
     //   this.filter();
     // });
 
-    this.$filters.forEach(select => {
-      select.addEventListener('change', () => {
+    this.$filters.forEach((select) => {
+      select.addEventListener("change", () => {
         this.filter();
       });
     });
@@ -44,29 +44,32 @@ export default class Grid {
   // }
 
   setEqualHeight() {
-    const cards = document.querySelectorAll('.js-grid .fr-card');
+    const cards = document.querySelectorAll(".js-grid .fr-card");
     var elems = [].slice.call(cards);
 
-    var tallest = Math.max.apply(Math, elems.map(function(elem, index) {
-      elem.style.minHeight = ''; // clean first
-      return elem.offsetHeight;
-    }));
-  
-    elems.forEach(function(elem, index, arr){ 
-      elem.style.minHeight = tallest + 'px';
+    var tallest = Math.max.apply(
+      Math,
+      elems.map(function(elem, index) {
+        elem.style.minHeight = ""; // clean first
+        return elem.offsetHeight;
+      })
+    );
+
+    elems.forEach(function(elem, index, arr) {
+      elem.style.minHeight = tallest + "px";
     });
   }
 
   filter() {
-    let filters = [];
-    this.$filters.forEach(select => {
-      if(select.value != '*') {
-        filters.push('.' + select.value);
+    const filters = [];
+    this.$filters.forEach((select) => {
+      if (select.value !== "*") {
+        filters.push("." + select.value);
       }
     });
 
     this.isotope.arrange({
-      filter: filters.length > 0 ? filters.join('') : '*'
+      filter: filters.length > 0 ? filters.join("") : "*",
     });
   }
 }
